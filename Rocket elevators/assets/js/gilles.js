12603f42281060f9607208cd7fe0de2282016c60
Gillesatrudel <42750038+Gillesatrudel@users.noreply.github.com>
2018-09-07 12:50:59 -0400

    avancement

--- Rocket elevators/assets/js/gilles.js
+++ Rocket elevators/assets/js/gilles.js
@@ -2,9 +2,9 @@ function quotation() {
  //Obtenir les valeurs saisis
  //calculer
  //reafficher le résultat
- const standard=  7565;
- const premium=  12345;
- const excelium= 15400; 
+ //const standard=  7565;
+ //const premium=  12345;
+// const excelium= 15400; 
  
 
  var number_apartment = parseInt(document.getElementById("number_apartment").value);
@@ -21,13 +21,11 @@ function quotation() {
  //var excellium = parseInt(document.getElementById(excellium).value);
  var building_type_obj = document.getElementById("building_type");
  var building_type = building_type_obj.options[building_type_obj.selectedIndex].value;
-
- //document.querySelector('input[name="select_gamme"]:checked').value;
- //if (select_gamme=="standard") { 
- //   7565*1.1
+// var nombre_suggere_type_obj = document.getElementById("Nombre_suggere");
+ //var nombre_suggere = nombre_suggere_type_obj.option[nombre_suggere_type_obj.selectedIndex].value;
 
 
- var e = document.getElementById("building_type");
+ 
  // returned_value["building_type"] = e[e.selectedindex].value;
  //RESIDENTIAL
 
@@ -48,23 +46,113 @@ function quotation() {
 
  } else {
     if (building_type=="commercial") {
-    document.getElementById("nombre_suggere").innerHTML = number_elevator
+     cageRequise = number_elevator
     } else {
         " "
     }
+
+    
+    
  }     
-
-
-
-                                       
- document.getElementById("nombre_suggere").innerHTML = cageRequise
-  
- }
+                                
+    document.getElementById("nombre_suggere").innerHTML = cageRequise
  
-
-
  
-  
+ var projectCost = 0;
+
+ var select_gamme = document.querySelector('input[name="select_gamme"]:checked').value;
+   if (select_gamme=="standard") { 
+    projectCost = Math.round(((7565*cageRequise)*1.1)*100)/100;
+    }  
+   if (select_gamme=="premium") {
+    projectCost = Math.round(((12345*cageRequise)*1.13)*100)/100;  
+    }  
+   if (select_gamme=="excellium")  {
+    projectCost = Math.ceil(15400*cageRequise)*1.16;
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+   }
+
+// var  = document.getElementById("totalPrice");
+
+document.getElementById("total_price").innerHTML = projectCost + "$";
+event.preventDefault();     
+}
+
+
+//function display_relevant_fields(selected_value){
+//    var building_type = selected_value.value;
+//    console.log("selected_value", selected_value.value);
+ 
+  //  if (building_type=="") {
+
+    function display_relevant_fields(selected_value){
+        var building_type = selected_value.value;
+       // console.log("selected_value", selected_value.value);
+      
+         var building_type = (selected_value.value || selected_value.options[selected.value.selectedIndex].value);
+         if (selected_value.value == "residential") {
+              document.getElementById("total_price").style.display = 'block';
+              document.getElementById("box_nb_apartment").style.display = 'block';
+              document.getElementById("box_nb_floor").style.display = 'block';
+              document.getElementById("box_nb_floorbasement").style.display = 'block';
+              document.getElementById("box_nb_store").style.display = 'none';
+              document.getElementById("box_nb_parking").style.display = 'none';
+              document.getElementById("box_nb_elevator").style.display = 'none';
+              document.getElementById("box_nb_office").style.display = 'none';
+              document.getElementById("box_nb_person/floor").style.display = 'none';
+              document.getElementById("box_nb_businesshour").style.display = 'none';
+      
+      
+         }  else if (selected_value.value == "commercial"){
+             document.getElementById("box_nb_businesshour").style.display = 'none';
+             document.getElementById("box_nb_person/floor").style.display = 'none';
+             document.getElementById("box_nb_office").style.display = 'none';
+             document.getElementById("box_nb_apartment").style.display = 'none';
+             document.getElementById("box_nb_elevator").style.display = 'block';
+             document.getElementById("box_nb_floor").style.display = 'block';
+             document.getElementById("box_nb_floorbasement").style.display = 'block';
+             document.getElementById("box_nb_parking").style.display = 'block';
+             document.getElementById("box_nb_store").style.display = 'block';
+      
+          } else  if (selected_value.value == "corporate"){
+             document.getElementById("box_nb_businesshour").style.display = 'none';
+             document.getElementById("box_nb_person/floor").style.display = 'block';
+             document.getElementById("box_nb_office").style.display = 'block';
+             document.getElementById("box_nb_apartment").style.display = 'none';
+             document.getElementById("box_nb_elevator").style.display = 'none';
+             document.getElementById("box_nb_floor").style.display = 'block';
+             document.getElementById("box_nb_floorbasement").style.display = 'block';
+             document.getElementById("box_nb_parking").style.display = 'block';
+             document.getElementById("box_nb_store").style.display = 'none';
+      
+          } else if (selected_value.value == "hybrid"){
+             document.getElementById("box_nb_businesshour").style.display = 'block';
+             document.getElementById("box_nb_person/floor").style.display = 'block';
+             document.getElementById("box_nb_office").style.display = 'none';
+             document.getElementById("box_nb_apartment").style.display = 'none';
+             document.getElementById("box_nb_elevator").style.display = 'none';
+             document.getElementById("box_nb_floor").style.display = 'block';
+             document.getElementById("box_nb_floorbasement").style.display = 'block';
+             document.getElementById("box_nb_parking").style.display = 'block';
+             document.getElementById("box_nb_store").style.display = 'block';
+          }
+   
+
+        }
+
+
 //COST installation standard= 10%
 //COST installation premium=  13%
 //COST installation excelium= 16%
